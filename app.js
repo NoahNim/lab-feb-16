c6e4c08154227ce472f41da2dc1fa77278feb75b
‘Jerry <jerryandhisemail@gmail.com>
2016-02-15 11:24:23 -0800

    adding completed problem2 branch

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ var sum = function (num1, num2) {
   console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + total);
   return total;
 }
+
+
+// Problem 2
+
+var multiply = function (num1, num2) {
+  totalMultiply = (num1 * num2);
+  console.log('The product of ' + num1 + ' and ' + num2 + ' is ' + totalMultiply);
+  return totalMultiply;
+}
